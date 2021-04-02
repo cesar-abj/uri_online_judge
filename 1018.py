011838f320c53e462d2e1cea7fccbd2aceaea102
Cesar Augusto <cesaraugustojun@gmail.com>
2021-04-01 23:56:24 -0300

    mais algumas resoluções

--- 1018.py
+++ 1018.py
@@ -16,10 +16,5 @@ for nota in notas:
     op -= nota * int(op / nota)
 
 print(valor)
-print(f'{notas[100]} nota(s) de R$ 100,00')
-print(f'{notas[50]} nota(s) de R$ 50,00')
-print(f'{notas[20]} nota(s) de R$ 20,00')
-print(f'{notas[10]} nota(s) de R$ 10,00')
-print(f'{notas[5]} nota(s) de R$ 5,00')
-print(f'{notas[2]} nota(s) de R$ 2,00')
-print(f'{notas[1]} nota(s) de R$ 1,00')
+for nota in notas: 
+    print(f'{notas[nota]} nota(s) de R$ {nota},00')
